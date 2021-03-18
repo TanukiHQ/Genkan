fb4a59ced03830930aaa35a43120b270a8d57a21
Dylan Kok <tenkotofu@gmail.com>
2021-03-19 01:40:11 +0800

    Lint fixes

--- genkan/auth/register.js
+++ genkan/auth/register.js
@@ -1,5 +1,5 @@
 // Load environment
-const config = require("../config")
+const config = require('../config')
 // Name of theme used in configuration
 const theme = `genkan-theme-${config.genkan.theme}`
 
@@ -20,105 +20,105 @@ const tokenGenerator = require('./tokenGenerator')
 // NodeMailer
 const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
-    host: config.smtp.server,
-    port: config.smtp.port,
-    auth: {
-        user: config.smtp.username,
-        pass: config.smtp.password
-    }
+  host: config.smtp.server,
+  port: config.smtp.port,
+  auth: {
+    user: config.smtp.username,
+    pass: config.smtp.password,
+  },
 });
 
 // Handlebars
-const Handlebars = require("handlebars")
+const Handlebars = require('handlebars')
 
 // Email Template
 const fs = require('fs')
 const confirmEmailSource = fs.readFileSync(`node_modules/${theme}/mail/confirmation.hbs`, 'utf8');
 const confirmEmailTemplate = Handlebars.compile(confirmEmailSource);
 
-MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    const db = client.db(dbName)
-    newAccount = (email, password, callback) => {
-        // Check for duplicate accounts
-        findDB(db, "users", { "email": email }, result => {
-            // Reject if duplicate
-            if (result.length !== 0) {
-                return callback(false)
-            }
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+  const db = client.db(dbName)
+  newAccount = (email, password, callback) => {
+    // Check for duplicate accounts
+    findDB(db, 'users', {'email': email}, (result) => {
+      // Reject if duplicate
+      if (result.length !== 0) {
+        return callback(false)
+      }
 
-            // SHA512 Hashing
-            var hashedPasswordSHA512 = sha512({
-                a: password,
-                b: email + config.genkan.secretKey
-            })
+      // SHA512 Hashing
+      const hashedPasswordSHA512 = sha512({
+        a: password,
+        b: email + config.genkan.secretKey,
+      })
 
-            // Bcrypt Hashing
-            var hashedPasswordSHA512Bcrypt = bcrypt.hashSync(hashedPasswordSHA512, saltRounds)
+      // Bcrypt Hashing
+      const hashedPasswordSHA512Bcrypt = bcrypt.hashSync(hashedPasswordSHA512, saltRounds)
 
-            // Generate email confirmation token
-            var emailConfirmationToken = tokenGenerator()
+      // Generate email confirmation token
+      const emailConfirmationToken = tokenGenerator()
 
-            const NewUserSchema = {
-                "email": email,
-                "password": hashedPasswordSHA512Bcrypt,
-                "account": {
-                    "activity": {
-                        "created": new Date(),
-                        "lastSeen": null
-                    },
-                    "type": "STANDARD",
-                    "suspended": false,
-                    "emailVerified": false
-                },
-                "tokens": {
-                    "emailConfirmation": emailConfirmationToken
-                }
-            }
+      const NewUserSchema = {
+        'email': email,
+        'password': hashedPasswordSHA512Bcrypt,
+        'account': {
+          'activity': {
+            'created': new Date(),
+            'lastSeen': null,
+          },
+          'type': 'STANDARD',
+          'suspended': false,
+          'emailVerified': false,
+        },
+        'tokens': {
+          'emailConfirmation': emailConfirmationToken,
+        },
+      }
 
-            // Insert new user into database
-            insertDB(db, "users", NewUserSchema, () => {
-                callback(true)
-                sendConfirmationEmail(email, emailConfirmationToken)
-            })
-        })
+      // Insert new user into database
+      insertDB(db, 'users', NewUserSchema, () => {
+        callback(true)
+        sendConfirmationEmail(email, emailConfirmationToken)
+      })
+    })
+  }
+
+  sendConfirmationEmail = (email, token) => {
+    // Compile from email template
+    const data = {
+      receiver: email,
+      url: `https://id.hakkou.app/register?confirmation=${token}`,
     }
+    const message = confirmEmailTemplate(data);
 
-    sendConfirmationEmail = (email, token) => {
-        // Compile from email template
-        var data = {
-            receiver: email,
-            url: `https://id.hakkou.app/register?confirmation=${token}`
-        }
-        var message = confirmEmailTemplate(data);
+    // send email
+    transporter.sendMail({
+      from: config.smtp.mailFromAddress,
+      to: email,
+      subject: 'Confirm your HakkouID',
+      html: message,
+    });
+  }
 
-        // send email
-        transporter.sendMail({
-            from: config.smtp.mailFromAddress,
-            to: email,
-            subject: 'Confirm your HakkouID',
-            html: message
-        });
-    }
+  confirmEmail = (token, callback) => {
+    findDB(db, 'users', {'tokens.emailConfirmation': token}, (result) => {
+      if (result.length !== 1) {
+        return callback(false)
+      }
+      const AccountActivatePayload = {
+        $unset: {
+          'tokens.emailConfirmation': true,
+        },
+        $set: {
+          'account.emailVerified': true,
+        },
+      }
 
-    confirmEmail = (token, callback) => {
-        findDB(db, "users", { "tokens.emailConfirmation": token }, result => {
-            if (result.length !== 1) {
-                return callback(false)
-            }
-            const AccountActivatePayload = {
-                $unset: {
-                    "tokens.emailConfirmation": true
-                },
-                $set: {
-                    "account.emailVerified": true
-                }
-            }
+      updateDB(db, 'users', {'tokens.emailConfirmation': token}, AccountActivatePayload, () => {
+        callback(true)
+      })
+    })
+  }
 
-            updateDB(db, "users", { "tokens.emailConfirmation": token }, AccountActivatePayload, () => {
-                callback(true)
-            })
-        })
-    }
-
-    module.exports = newAccount
+  module.exports = newAccount
 })
--- genkan/db.js
+++ genkan/db.js
@@ -1,43 +1,43 @@
-insertDB = function (db, coll, docs, callback) {
-    // Get the documents collection
-    const collection = db.collection(coll)
-    // Insert some documents
-    collection.insertMany([
-        docs
-    ], function (err, result) {
-        if (err) throw err
-        callback(result)
-    })
+insertDB = function(db, coll, docs, callback) {
+  // Get the documents collection
+  const collection = db.collection(coll)
+  // Insert some documents
+  collection.insertMany([
+    docs,
+  ], function(err, result) {
+    if (err) throw err
+    callback(result)
+  })
 }
 
-updateDB = function (db, coll, query, ops, callback) {
-    // Get the documents collection
-    const collection = db.collection(coll)
-    // Update document where a is 2, set b equal to 1
-    collection.updateOne(query, ops, function (err, result) {
-        if (err) throw err
-        callback(result)
-    })
+updateDB = function(db, coll, query, ops, callback) {
+  // Get the documents collection
+  const collection = db.collection(coll)
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne(query, ops, function(err, result) {
+    if (err) throw err
+    callback(result)
+  })
 }
 
-findDB = function (db, coll, query, callback) {
-    // Get the documents collection
-    const collection = db.collection(coll)
-    // Find some documents
-    collection.find(query).toArray(function (err, docs) {
-        if (err) throw err
-        callback(docs)
-    })
+findDB = function(db, coll, query, callback) {
+  // Get the documents collection
+  const collection = db.collection(coll)
+  // Find some documents
+  collection.find(query).toArray(function(err, docs) {
+    if (err) throw err
+    callback(docs)
+  })
 }
 
-deleteDB = function (db, coll, query, callback) {
-    // Get the documents collection
-    const collection = db.collection(coll)
-    // Find some documents
-    collection.remove(query).toArray(function (err) {
-        if (err) throw err
-        callback(true)
-    })
+deleteDB = function(db, coll, query, callback) {
+  // Get the documents collection
+  const collection = db.collection(coll)
+  // Find some documents
+  collection.remove(query).toArray(function(err) {
+    if (err) throw err
+    callback(true)
+  })
 }
 
 module.exports = insertDB
