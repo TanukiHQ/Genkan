87e9c46672915584124b3955f4eeee4302a8ec48
Dylan Kok <tenkotofu@gmail.com>
2021-03-19 01:18:18 +0800

    Added copyright disclaimer in boot splash

--- genkan/boot.js
+++ genkan/boot.js
@@ -13,7 +13,7 @@ module.exports = {
 | |__| ||  __/| | | ||   <| (_| || | | |
  \\_____| \\___||_| |_||_|\\_\\\\__,_||_| |_|`)}
 
-${chalk.whiteBright(`Genkan 玄関`)}
+${chalk.whiteBright(`Genkan 玄関`)} | ${chalk.green('Made with ❤ by Hakkou.app (Read LICENSE for more info)')}
 ${chalk.grey(`Version: ${chalk.bold(chalk.magenta(`${package.version}`))}`)}
 `)
     }
