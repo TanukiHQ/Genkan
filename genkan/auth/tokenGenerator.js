ca619c85fc8f0c552a7ff2656a014909d36534c7
Dylan Kok <tenkotofu@gmail.com>
2021-03-19 01:17:53 +0800

    Added apiKeyGenerator to tokenGenerator.js

--- genkan/auth/tokenGenerator.js
+++ genkan/auth/tokenGenerator.js
@@ -1,6 +1,7 @@
 // UUID & Hashing
 const uuid = require('uuid')
 const sha512 = require('hash-anything').sha512
+const sha1 = require('hash-anything').sha1
 
 tokenGenerator = () => {
     // Generate and return (sync) random sha512 string
@@ -10,4 +11,13 @@ tokenGenerator = () => {
     })
 }
 
+apiKeyGenerator = () => {
+    // Generate and return (sync) random sha1 string
+    return `genkan_${sha1({
+        a: tokenGenerator(),
+        b: tokenGenerator() + (new Date()).toISOString()
+    })}`
+}
+
 module.exports = tokenGenerator
+module.exports = apiKeyGenerator
