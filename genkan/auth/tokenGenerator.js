4de5077b2f1a9daf9f9166c7fc0dcbb2299625b8
Dylan Kok <tenkotofu@gmail.com>
2021-03-20 01:15:29 +0800

    Removed sha1 dependency require since no longer in use

--- genkan/auth/tokenGenerator.js
+++ genkan/auth/tokenGenerator.js
@@ -1,7 +1,6 @@
 // UUID & Hashing
 const uuid = require('uuid')
 const sha512 = require('hash-anything').sha512
-const sha1 = require('hash-anything').sha1
 
 tokenGenerator = () => {
     // Generate and return (sync) random sha512 string
